55a3b309e4d918eb337ab92b64ba8087aa94b1d2
João Matheus <xdjohnmath@gmail.com>
2017-09-21 22:45:40 -0300

    Projeto Criado

---
+++ XadrezConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace XadrezConsole {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
