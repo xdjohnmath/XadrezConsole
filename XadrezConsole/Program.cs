6f0e20942f341b8f8ee3f4d5e02d786892bf14de
João Matheus <xdjohnmath@gmail.com>
2017-09-21 23:56:13 -0300

    Tratando erros com exceção personalizada

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -5,14 +5,17 @@ using xadrex;
 namespace XadrezConsole {
     class Program {
         static void Main(string[] args) {
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(tab);
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.Read();
         }
--- XadrezConsole/tabuleiro/Tabuleiro.cs
+++ XadrezConsole/tabuleiro/Tabuleiro.cs
@@ -16,9 +16,37 @@
             return pecas [linha, coluna];               //Retorna o objeto privado como público através do método criado.
         }
 
+        public Peca peca (Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool ExistePeca(Posicao pos) {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos) {
+            if (ExistePeca(pos)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+
+        public bool PosicaoValida(Posicao pos) {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas) {
+                return false; 
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos) {
+            if (!PosicaoValida(pos)) {
+                throw new TabuleiroException("Posição Inválida!");
+
+            }
+        }
+
+
     }
 }
